118f7a87a773760d01f29482e95a5522385a607f
josephine19001 <gyameraj@gmail.com>
2024-10-19 02:57:58 +0300

    Fixed create-checkout

--- app/[locale]/api/create-checkout-session/route.ts
+++ app/[locale]/api/create-checkout-session/route.ts
@@ -1,30 +1,38 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === 'POST') {
-    try {
-      const { items } = req.body;
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { items } = req.body;
 
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: items,
-        mode: 'payment',
-        success_url: `${req.headers.origin}/payment-success`,
-        cancel_url: `${req.headers.origin}/payment-failure`
-      });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: items,
+      mode: 'payment',
+      success_url: `${req.headers.origin}/payment-success`,
+      cancel_url: `${req.headers.origin}/payment-failure`
+    });
 
-      res.status(200).json({ sessionId: session.id });
-    } catch (err: any) {
-      res.status(err.statusCode || 500).json(err.message);
-    }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    NextResponse.json(
+      {
+        message: 'Session created successfully'
+      },
+      {
+        status: 200
+      }
+    );
+  } catch (err: any) {
+    console.error('Error creating checkout session:', err);
+    NextResponse.json(
+      {
+        message: 'Failed to create checkout session'
+      },
+      {
+        status: 500
+      }
+    );
   }
 }
