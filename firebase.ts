4360d3629f4c0659f11c64eb6210a703e0072f80
Stanley Hayford <stanley.hayford@betika.com>
2024-10-27 12:36:30 +0000

    Add firebase realtime database

--- firebase.ts
+++ firebase.ts
@@ -1,16 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+import {getStorage} from 'firebase/storage';
+import {getDatabase} from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,23 +19,24 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const realtimeDatabase = getDatabase(app);
 
 let messaging = null;
 let getToken = null;
 let onMessage = null;
 
 if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-  import('firebase/messaging')
-    .then(
-      ({ getMessaging, getToken: fetchToken, onMessage: handleOnMessage }) => {
-        messaging = getMessaging(app);
-        getToken = fetchToken;
-        onMessage = handleOnMessage;
-      }
-    )
-    .catch((error) => {
-      console.error('Firebase Messaging initialization failed:', error);
-    });
+    import('firebase/messaging')
+        .then(
+            ({getMessaging, getToken: fetchToken, onMessage: handleOnMessage}) => {
+                messaging = getMessaging(app);
+                getToken = fetchToken;
+                onMessage = handleOnMessage;
+            }
+        )
+        .catch((error) => {
+            console.error('Firebase Messaging initialization failed:', error);
+        });
 }
 
-export { messaging, getToken, onMessage };
+export {messaging, getToken, onMessage};
--- store/slices/chat-slice.ts
+++ store/slices/chat-slice.ts
@@ -1,158 +1,185 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import {
-  doc,
-  updateDoc,
-  getDoc,
-  addDoc,
-  collection,
-  Timestamp,
-  setDoc
-} from 'firebase/firestore';
-import { db, storage } from '../../firebase';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { Message } from 'components/types';
-import { toast } from 'react-toastify';
-import { convertTimestampToISO } from './orders-slice';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {doc, getDoc, setDoc, Timestamp, updateDoc} from 'firebase/firestore';
+import {db, realtimeDatabase, storage} from '../../firebase';
+import {getDownloadURL, ref, uploadBytes} from 'firebase/storage';
+import {Message} from 'components/types';
+import {toast} from 'react-toastify';
+import {convertTimestampToISO} from './orders-slice';
+import database from 'firebase/database';
 
 export const fetchChat = createAsyncThunk(
-  'chat/fetchChat',
-  async (userId: string, { rejectWithValue }) => {
-    try {
-      const chatDocRef = doc(db, 'chats', userId);
-      const chatSnap = await getDoc(chatDocRef);
-      if (chatSnap.exists()) {
-        const data = chatSnap.data();
-        return convertTimestampToISO(data);
-      } else {
-        const newChat = {
-          userId,
-          createdAt: Timestamp.now(),
-          messages: []
-        };
-        await addDoc(collection(db, 'chats'), newChat);
-        return newChat;
-      }
-    } catch (error) {
-      toast.error('Error fetching chat');
-      return rejectWithValue('Error fetching chat');
+    'chat/fetchChat',
+    async (userId: string, {rejectWithValue}) => {
+        try {
+            ////////////
+            const chatsRef = database.ref(realtimeDatabase, `chats/${userId}`);
+
+            database.onValue(chatsRef, (snapshot: database.DataSnapshot) => {
+                const data = snapshot.val();
+                return convertTimestampToISO(data);
+            })
+
+            const newChat = {
+                userId,
+                createdAt: Timestamp.now().toString(),
+                messages: []
+            };
+            newChat['chatId'] = database.push(database.child(database.ref(realtimeDatabase), 'chats')).key;
+            return newChat;
+            ////////////
+
+            // const chatDocRef = doc(db, 'chats', userId);
+            // const chatSnap = await getDoc(chatDocRef);
+            // if (chatSnap.exists()) {
+            //   const data = chatSnap.data();
+            //   return convertTimestampToISO(data);
+            // } else {
+            //   const newChat = {
+            //     userId,
+            //     createdAt: Timestamp.now(),
+            //     messages: []
+            //   };
+            //   await addDoc(collection(db, 'chats'), newChat);
+            //   return newChat;
+            // }
+        } catch (error) {
+            toast.error('Error fetching chat');
+            return rejectWithValue('Error fetching chat');
+        }
     }
-  }
 );
 
 export const sendMessage = createAsyncThunk(
-  'chat/sendMessage',
-  async (
-    {
-      userId,
-      content,
-      images,
-      sender,
-      isAutoReply
-    }: {
-      userId: string;
-      content: string;
-      images: File[];
-      sender: string;
-      isAutoReply?: boolean;
-    },
-    { rejectWithValue }
-  ) => {
-    try {
-      const chatDocRef = doc(db, 'chats', userId);
+    'chat/sendMessage',
+    async (
+        {
+            userId,
+            content,
+            images,
+            sender,
+            isAutoReply,
+            chatId
+        }: {
+            userId: string;
+            content: string;
+            images: File[];
+            sender: string;
+            isAutoReply?: boolean;
+            chatId: string
+        },
+        {rejectWithValue}
+    ) => {
+        try {
+            const chatDocRef = doc(db, 'chats', userId);
+            const message: Message = {
+                sender,
+                content,
+                time: new Date().toLocaleTimeString(),
+                imageUrls: [],
+                isRead: false
+            };
 
-      const message: Message = {
-        sender,
-        content,
-        time: new Date().toLocaleTimeString(),
-        imageUrls: [],
-        isRead: false
-      };
+            let uploadedImageUrls: string[] = [];
+            if (images && images.length > 0) {
+                const uploadPromises = images.map(async (image) => {
+                    const imageRef = ref(storage, `chats-images/${userId}/${image.name}`);
+                    await uploadBytes(imageRef, image);
+                    return await getDownloadURL(imageRef);
+                });
+                uploadedImageUrls = await Promise.all(uploadPromises);
+            }
 
-      let uploadedImageUrls: string[] = [];
-      if (images && images.length > 0) {
-        const uploadPromises = images.map(async (image) => {
-          const imageRef = ref(storage, `chats-images/${userId}/${image.name}`);
-          await uploadBytes(imageRef, image);
-          const downloadUrl = await getDownloadURL(imageRef);
-          return downloadUrl;
-        });
-        uploadedImageUrls = await Promise.all(uploadPromises);
-      }
+            const updatedMessage: Message = {
+                ...message,
+                imageUrls: uploadedImageUrls
+            };
 
-      const updatedMessage: Message = {
-        ...message,
-        imageUrls: uploadedImageUrls
-      };
+            const chatSnap = await getDoc(chatDocRef);
 
-      const chatSnap = await getDoc(chatDocRef);
+            if (chatSnap.exists()) {
+                const chatData = chatSnap.data();
 
-      if (chatSnap.exists()) {
-        const chatData = chatSnap.data();
+                if (sender === 'admin') {
+                    if (!chatData.welcomeMessageSent) {
+                        const updatedMessages = [...chatData.messages, updatedMessage];
+                        await updateDoc(chatDocRef, {
+                            messages: updatedMessages,
+                            welcomeMessageSent: true
+                        });
+                        return updatedMessage;
+                    }
+                } else {
+                    const updatedMessages = [...chatData.messages, updatedMessage];
+                    await updateDoc(chatDocRef, {messages: updatedMessages});
+                    return updatedMessage;
+                }
+            } else {
+                const newChat = {
+                    userId,
+                    createdAt: new Date(),
+                    messages: [updatedMessage],
+                    welcomeMessageSent: sender === 'admin' // Set the flag based on the sender
+                };
+                await setDoc(chatDocRef, newChat);
+                return updatedMessage;
+            }
+            // uploading the images and sending the message should be in a transaction
+            // get reference to messages/chatId
+            // push the message into the above reference
 
-        if (sender === 'admin') {
-          if (!chatData.welcomeMessageSent) {
-            const updatedMessages = [...chatData.messages, updatedMessage];
-            await updateDoc(chatDocRef, {
-              messages: updatedMessages,
-              welcomeMessageSent: true
+            // Step 4: Run transaction to add the message to Firebase Realtime Database.
+            const messageRef = database.ref(realtimeDatabase, `messages/${chatId}`);
+            const result = await database.runTransaction(messageRef, (messages) => {
+                if (!messages) {
+                    messages = [];
+                }
+                messages.push(updatedMessage);
+                return messages;
             });
-            return updatedMessage;
-          }
-        } else {
-          const updatedMessages = [...chatData.messages, updatedMessage];
-          await updateDoc(chatDocRef, { messages: updatedMessages });
-          return updatedMessage;
+            console.log('Result', result);
+            return result.toJSON();
+        } catch (error) {
+            console.log('Error sending message', error);
+            toast.error('Error sending message');
+            return rejectWithValue('Error sending message');
         }
-      } else {
-        const newChat = {
-          userId,
-          createdAt: new Date(),
-          messages: [updatedMessage],
-          welcomeMessageSent: sender === 'admin' ? true : false // Set the flag based on the sender
-        };
-        await setDoc(chatDocRef, newChat);
-        return updatedMessage;
-      }
-    } catch (error) {
-      console.log('Error sending message', error);
-      toast.error('Error sending message');
-      return rejectWithValue('Error sending message');
     }
-  }
 );
 
 const chatSlice = createSlice({
-  name: 'chat',
-  initialState: {
-    chat: null,
-    messages: [],
-    loading: false,
-    error: null
-  },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchChat.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(fetchChat.fulfilled, (state, action) => {
-        state.loading = false;
-        state.chat = action.payload;
-        state.messages = action.payload.messages || [];
-      })
-      .addCase(fetchChat.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-        toast.error('Failed to fetch chat');
-      })
-      .addCase(sendMessage.fulfilled, (state, action) => {
-        state.messages.push(action.payload);
-      })
-      .addCase(sendMessage.rejected, (state, action) => {
-        state.error = action.payload;
-        toast.error('Failed to send message');
-      });
-  }
+    name: 'chat',
+    initialState: {
+        chat: null,
+        messages: [],
+        loading: false,
+        error: null
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchChat.pending, (state) => {
+                console.log('Fetch chat pending');
+                state.loading = true;
+            })
+            .addCase(fetchChat.fulfilled, (state, action) => {
+                console.log('Fetch chat fulfilled', action.payload);
+                state.loading = false;
+                state.chat = action.payload;
+                state.messages = action.payload.messages || [];
+            })
+            .addCase(fetchChat.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                toast.error('Failed to fetch chat');
+            })
+            .addCase(sendMessage.fulfilled, (state, action) => {
+                state.messages.push(action.payload);
+            })
+            .addCase(sendMessage.rejected, (state, action) => {
+                state.error = action.payload;
+                toast.error('Failed to send message');
+            });
+    }
 });
 
 export default chatSlice.reducer;
