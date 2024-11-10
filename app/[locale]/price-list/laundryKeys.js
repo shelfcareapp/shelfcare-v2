35765a1092e5e030feaf49b45773beccb2b37ed6
josephine19001 <gyameraj@gmail.com>
2024-11-10 21:25:30 +0200

    Fixed missing laundry

--- app/[locale]/price-list/laundryKeys.js
+++ app/[locale]/price-list/laundryKeys.js
@@ -1,13 +1,16 @@
-// laundryKeys.js
 export const laundryKeys = [
   'trousers',
   'skirt',
   'suit',
+  'suit_vest',
   'dress_mini',
   'dress_midi',
   'dress_maxi',
   'evening_dress',
   'jacket',
+  'tie',
+  'shirt',
+  'scarf',
   'coat',
   'winter_jacket',
   'down_jacket',
@@ -19,5 +22,7 @@ export const laundrySubOptionsKeys = {
   suit: ['silk_suit'],
   coat: ['wool_coat', 'length_over_knee'],
   winter_jacket: ['length_over_knee'],
-  down_jacket: ['length_over_knee']
+  down_jacket: ['length_over_knee'],
+  scarf: ['silk_scarf'],
+  shirt: ['silk_shirt']
 };
